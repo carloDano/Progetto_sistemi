d11cd0055aa25bcdcb915b2685652581698932c3
Dano Carlo <carlo.dano@cz.lan>
2025-04-08 13:42:41 +0200

    Cerazione del server 2

--- Progetto/src/MainServer.java
+++ Progetto/src/MainServer.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class MainServer {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class MainServer {
+    public static void main(String[] args) {
+        int port = 12345;
+        String filePath = "\\C:\\Users\\carlo.dano\\Documents\\Progetto_sistemi\\Progetto\\Mappa-agriturismo-BB-locanda-e-simili-in-Italia.csv";
+
+        try {
+            CSVHandler csvHandler = new CSVHandler(filePath);
+            ServerSocket serverSocket = new ServerSocket(port);
+            System.out.println("Server avviato sulla porta " + port);
+
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Nuovo client connesso.");
+                ClientHandler clientHandler = new ClientHandler(clientSocket, csvHandler);
+                clientHandler.start();
+            }
+
+        } catch (IOException e) {
+            System.err.println("Errore nell'avvio del server: " + e.getMessage());
+        }
+    }
 }
